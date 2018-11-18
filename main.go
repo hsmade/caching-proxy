8c002b8a38240597c8746d629627b24a7493c721
Wim Fournier <wifournier@ebay.com>
2018-11-18 23:03:33 +0100

    fmt

--- main.go
+++ main.go
@@ -1,14 +1,14 @@
 package main
 
 import (
+	"bytes"
+	"flag"
 	"github.com/elazarl/goproxy"
+	"io"
+	"io/ioutil"
 	"log"
 	"net/http"
-	"io/ioutil"
-	"bytes"
-	"io"
 	"sync"
-	"flag"
 )
 
 func copyBody(source io.ReadCloser, ctx *goproxy.ProxyCtx) (dest1 io.ReadCloser, dest2 io.ReadCloser) {
@@ -22,10 +22,9 @@ func copyBody(source io.ReadCloser, ctx *goproxy.ProxyCtx) (dest1 io.ReadCloser,
 }
 
 var (
-	port string
+	port    string
 	verbose bool
 )
-
 
 func main() {
 	flag.StringVar(&port, "port", "3128", "Port to listen on")
@@ -40,7 +39,8 @@ func main() {
 	proxy.OnRequest().DoFunc(
 		func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
 			ctx.Logf("Handling request for %v", r.URL.String())
-			resp, ok := cache[r.URL.String()]; if ok {
+			resp, ok := cache[r.URL.String()]
+			if ok {
 				ctx.Warnf("Returning cached data[%v] for %v", len(cache), r.URL.String())
 				responseCopy := *resp
 				responseCopy.Body, resp.Body = copyBody(resp.Body, ctx)
@@ -52,17 +52,18 @@ func main() {
 	proxy.OnResponse().DoFunc(func(resp *http.Response, ctx *goproxy.ProxyCtx) *http.Response {
 		if ctx.Error != nil {
 			ctx.Warnf("RETRYING because of %v", ctx.Error)
-			resp, _ = http.DefaultClient.Do(ctx.Req)  // retry
+			resp, _ = http.DefaultClient.Do(ctx.Req) // retry
 		} else if resp.StatusCode >= 500 {
 			ctx.Warnf("RETRYING because of status code: %v", resp.StatusCode)
-			resp, _ = http.DefaultClient.Do(ctx.Req)  // retry
+			resp, _ = http.DefaultClient.Do(ctx.Req) // retry
 		}
 
 		if resp.StatusCode >= 500 || ctx.Error != nil {
 			ctx.Warnf("Returning errored response after retry")
-			return resp  // Don't cache this
+			return resp // Don't cache this
 		}
-		_, ok := cache[ctx.Req.URL.String()]; if !ok {
+		_, ok := cache[ctx.Req.URL.String()]
+		if !ok {
 			ctx.Warnf("Storing response in cache[%v] for %v", len(cache), ctx.Req.URL.String())
 			responseCopy := *resp
 			responseCopy.Body, resp.Body = copyBody(resp.Body, ctx)
@@ -75,5 +76,5 @@ func main() {
 
 	proxy.Verbose = verbose
 	log.Printf("Starting proxy on :%v", port)
-	log.Fatalln(http.ListenAndServe(":" + port, proxy))
+	log.Fatalln(http.ListenAndServe(":"+port, proxy))
 }
