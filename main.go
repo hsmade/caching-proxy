93dc97ff6784ed18d7902c11d29467926c52bf87
Wim Fournier <wifournier@ebay.com>
2018-11-27 22:30:13 +0100

    improve handling of body

--- main.go
+++ main.go
@@ -2,34 +2,48 @@ package main
 
 import (
 	"bytes"
+	"crypto/md5"
+	"encoding/hex"
 	"flag"
 	"github.com/elazarl/goproxy"
-	"io"
 	"io/ioutil"
 	"log"
 	"net/http"
 	"sync"
-	"crypto/md5"
-	"encoding/hex"
 	"time"
 )
 
-func copyBody(source io.ReadCloser, ctx *goproxy.ProxyCtx) (dest1 io.ReadCloser, dest2 io.ReadCloser) {
-	buf, err := ioutil.ReadAll(source)
+var (
+	port        string
+	verbose     bool
+	maxRretries int
+	retrySleep  time.Duration
+)
+
+type cacheItem struct {
+	response *http.Response
+	body     *[]byte
+}
+
+func extractBody(source *http.Response, ctx *goproxy.ProxyCtx) (data *[]byte) {
+	buf, err := ioutil.ReadAll(source.Body)
 	if err != nil {
 		ctx.Warnf("Failed to read body: %v", err)
 	}
-	dest1 = ioutil.NopCloser(bytes.NewBuffer(buf))
-	dest2 = ioutil.NopCloser(bytes.NewBuffer(buf))
+	err = source.Body.Close()
+	if err != nil {
+		ctx.Warnf("Failed to close body: %v", err)
+	}
+	data = &buf
+	source.Body = ioutil.NopCloser(bytes.NewReader(buf))
 	return
 }
 
-var (
-	port    string
-	verbose bool
-	maxRretries int
-	retrySleep time.Duration
-)
+func copyResponse(r *http.Response, b *[]byte) *http.Response {
+	c := *r
+	c.Body = ioutil.NopCloser(bytes.NewReader(*b))
+	return &c
+}
 
 func generateHash(r *http.Request) string {
 	hash := md5.New()
@@ -40,10 +54,10 @@ func main() {
 	flag.StringVar(&port, "port", "3128", "Port to listen on")
 	flag.BoolVar(&verbose, "verbose", false, "Verbose logging on")
 	flag.IntVar(&maxRretries, "max-retries", 30, "Max retries per call when a 500 is received")
-	flag.DurationVar(&retrySleep, "retry-sleep", 1 * time.Second, "Time to sleep in between retries")
+	flag.DurationVar(&retrySleep, "retry-sleep", 1*time.Second, "Time to sleep in between retries")
 	flag.Parse()
 
-	cache := make(map[string]*http.Response, 1000)
+	cache := make(map[string]cacheItem, 1000)
 
 	cacheWriteLock := sync.Mutex{}
 	proxy := goproxy.NewProxyHttpServer()
@@ -52,14 +66,13 @@ func main() {
 	proxy.OnRequest().DoFunc(
 		func(r *http.Request, ctx *goproxy.ProxyCtx) (*http.Request, *http.Response) {
 			hash := generateHash(r)
-			resp, ok := cache[hash]
+			cacheWriteLock.Lock()
+			defer cacheWriteLock.Unlock()
+			item, ok := cache[hash]
 			if ok {
 				ctx.Warnf("Returning cached data[%v] for %v", len(cache), r.URL.String())
-				cacheWriteLock.Lock()
-				responseCopy := *resp
-				responseCopy.Body, resp.Body = copyBody(resp.Body, ctx)
-				cacheWriteLock.Unlock()
-				return r, &responseCopy
+				response := copyResponse(item.response, item.body)
+				return r, response
 			}
 			return r, nil
 		})
@@ -75,7 +88,7 @@ func main() {
 				if resp.StatusCode != 500 {
 					break
 				}
-				retries ++
+				retries++
 			}
 		}
 
@@ -85,13 +98,14 @@ func main() {
 		}
 
 		cacheWriteLock.Lock()
+		defer cacheWriteLock.Unlock()
+
 		_, ok := cache[hash]
 		if !ok {
 			responseCopy := *resp
-			responseCopy.Body, resp.Body = copyBody(resp.Body, ctx)
-			cache[hash] = &responseCopy
+			responseBody := extractBody(resp, ctx)
+			cache[hash] = cacheItem{&responseCopy, responseBody}
 		}
-		cacheWriteLock.Unlock()
 		return resp
 	})
 
